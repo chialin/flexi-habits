987ba2a3921a0b948394c2726e6015a6ac8454eb
chialin <chialin.shr@gmail.com>
2024-04-30 21:18:53 +0800

    Apply change theme function on App

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { useTheme } from './hooks/useTheme';
 
 function App() {
   const [count, setCount] = useState(0);
+  const { theme, toggleTheme } = useTheme();
 
   return (
     <>
@@ -21,13 +23,15 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button
+          onClick={() => {
+            toggleTheme();
+          }}
+        >
+          change theme
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="read-the-docs">This theme is: {theme}</div>
     </>
   );
 }
