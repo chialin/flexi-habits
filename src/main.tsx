983b416a16404150e38bdc9175989b7d7db662a1
chialin <chialin.shr@gmail.com>
2024-05-11 15:07:38 +0800

    import bluma default css for theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
+import 'bulma/css/bulma.min.css';
 import { ThemeProvider } from './contexts/ThemeContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
